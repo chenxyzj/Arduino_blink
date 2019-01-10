ad3e722400157c446d7260893bc98bafdfbe33cf
chenxyzj <chenxyzj@163.com>
2019-01-10 14:44:18 +0800

    原始arduino代码导入，编译、下载、验证通过，首次提交。

---
+++ src/sketch_aug22a.ino
@@ -0,0 +1,15 @@
+int potPin = 7;    
+int ledPin = 13;      
+int val = 0;         
+
+void setup() {
+  pinMode(ledPin, OUTPUT);  
+}
+
+void loop() {
+  val = analogRead(potPin);    
+  digitalWrite(ledPin, HIGH);  // Open the LED.
+  delay(val);                
+  digitalWrite(ledPin, LOW);   // Close the LED.
+  delay(val);                
+}
